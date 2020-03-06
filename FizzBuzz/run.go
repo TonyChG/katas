bc09c6a701456798666f72956a5b3632d023603c
TonyChG <tonychg7@pm.me>
2020-03-06 17:47:25 +0100

    BLUE - Adapt FizzBuzz logic

--- FizzBuzz/run.go
+++ FizzBuzz/run.go
@@ -1,12 +1,19 @@
 package FizzBuzz
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Run(number int) (output string) {
 	output = ""
-	if number%3 == 0 && number%5 == 0 {
+	converted := strconv.Itoa(number)
+
+	if (number%3 == 0 && number%5 == 0) || (strings.ContainsAny(converted, "3") && strings.ContainsAny(converted, "5")) {
 		output = "FizzBuzz"
-	} else if number%3 == 0 {
+	} else if number%3 == 0 || strings.ContainsAny(converted, "3") {
 		output = "Fizz"
-	} else if number%5 == 0 {
+	} else if number%5 == 0 || strings.ContainsAny(converted, "5") {
 		output = "Buzz"
 	}
 	return
