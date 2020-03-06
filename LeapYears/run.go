d0a398371d3ec7bfd91a72eeb7bb6445ba657713
TonyChG <tonychg7@pm.me>
2020-03-06 18:07:27 +0100

    BLUE - Refacto LeapYears

--- LeapYears/run.go
+++ LeapYears/run.go
@@ -1,8 +1,15 @@
 package LeapYears
 
+import "strconv"
+
 func IsLeapYear(year int) bool {
 	if (year%400 == 0) || (year%4 == 0 && year%100 != 0) {
 		return true
 	}
 	return false
+}
+
+func IsLeapYearString(year string) (bool, error) {
+	c, err := strconv.Atoi(year)
+	return IsLeapYear(c), err
 }
--- LeapYears/run_test.go
+++ LeapYears/run_test.go
@@ -9,8 +9,16 @@ func AssertLeapYears(t *testing.T, input int, expectedOutput bool) {
 	}
 }
 
+func AssertLeapYearsString(t *testing.T, input string, expectedOutput bool) {
+	output, _ := IsLeapYearString(input)
+	if output != expectedOutput {
+		t.Errorf("LeapYears.IsLeapYearString(%s) = %v; want %v", input, output, expectedOutput)
+	}
+}
+
 func TestDivisibleByFourHundred(t *testing.T) {
 	AssertLeapYears(t, 2000, true)
+	AssertLeapYearsString(t, "2000", true)
 }
 
 func TestDivisibleByOneHundredNotFourHundred(t *testing.T) {
@@ -18,16 +26,26 @@ func TestDivisibleByOneHundredNotFourHundred(t *testing.T) {
 	AssertLeapYears(t, 1800, false)
 	AssertLeapYears(t, 1900, false)
 	AssertLeapYears(t, 2100, false)
+	AssertLeapYearsString(t, "1700", false)
+	AssertLeapYearsString(t, "1800", false)
+	AssertLeapYearsString(t, "1900", false)
+	AssertLeapYearsString(t, "2100", false)
 }
 
 func TestDivisibleByFourNotOneHundred(t *testing.T) {
 	AssertLeapYears(t, 2008, true)
 	AssertLeapYears(t, 2012, true)
 	AssertLeapYears(t, 2016, true)
+	AssertLeapYearsString(t, "2008", true)
+	AssertLeapYearsString(t, "2012", true)
+	AssertLeapYearsString(t, "2016", true)
 }
 
 func TestDivisibleNotByFour(t *testing.T) {
 	AssertLeapYears(t, 2017, false)
 	AssertLeapYears(t, 2018, false)
 	AssertLeapYears(t, 2019, false)
+	AssertLeapYearsString(t, "2017", false)
+	AssertLeapYearsString(t, "2018", false)
+	AssertLeapYearsString(t, "2019", false)
 }
