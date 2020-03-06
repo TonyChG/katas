3763778a6e4ebc8b1aef779ff629065b737129be
TonyChG <tonychg7@pm.me>
2020-03-06 17:47:07 +0100

    BLUE - FizzBuzz update tests

--- FizzBuzz/run_test.go
+++ FizzBuzz/run_test.go
@@ -2,38 +2,37 @@ package FizzBuzz
 
 import "testing"
 
-func TestEmptyOutput(t *testing.T) {
-	input := 1
-	expectedOutput := ""
+func AssertFizzBuzz(t *testing.T, input int, expectedOutput string) {
 	output := Run(input)
 	if output != expectedOutput {
 		t.Errorf("FizzBuzz.Run(%d) = %s; want %s", input, output, expectedOutput)
 	}
+}
+
+func TestEmptyOutput(t *testing.T) {
+	AssertFizzBuzz(t, 1, "")
 }
 
 func TestThreeToFizz(t *testing.T) {
-	input := 3
-	expectedOutput := "Fizz"
-	output := Run(input)
-	if output != expectedOutput {
-		t.Errorf("FizzBuzz.Run(%d) = %s; want %s", input, output, expectedOutput)
-	}
+	AssertFizzBuzz(t, 3, "Fizz")
 }
 
 func TestFiveToFizz(t *testing.T) {
-	input := 5
-	expectedOutput := "Buzz"
-	output := Run(input)
-	if output != expectedOutput {
-		t.Errorf("FizzBuzz.Run(%d) = %s; want %s", input, output, expectedOutput)
-	}
+	AssertFizzBuzz(t, 5, "Buzz")
 }
 
 func TestFifteenToFizzBuzz(t *testing.T) {
-	input := 15
-	expectedOutput := "FizzBuzz"
-	output := Run(input)
-	if output != expectedOutput {
-		t.Errorf("FizzBuzz.Run(%d) = %s; want %s", input, output, expectedOutput)
-	}
+	AssertFizzBuzz(t, 15, "FizzBuzz")
+}
+
+func TestThreeDigitToFizz(t *testing.T) {
+	AssertFizzBuzz(t, 43, "Fizz")
+}
+
+func TestFiveDigitToBuzz(t *testing.T) {
+	AssertFizzBuzz(t, 52, "Buzz")
+}
+
+func TestFiveThreeDigitToFizzBuzz(t *testing.T) {
+	AssertFizzBuzz(t, 53, "FizzBuzz")
 }
