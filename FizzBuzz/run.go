33713deeab52b68acc5f2a12a413de7f2fe97d7a
TonyChG <tonychg7@pm.me>
2020-03-06 17:33:30 +0100

    GREEN - Add FizzBuzz logic

--- FizzBuzz/run.go
+++ FizzBuzz/run.go
@@ -1,6 +1,13 @@
 package FizzBuzz
 
 func Run(number int) (output string) {
-	output = "bb"
+	output = ""
+	if number%3 == 0 && number%5 == 0 {
+		output = "FizzBuzz"
+	} else if number%3 == 0 {
+		output = "Fizz"
+	} else if number%5 == 0 {
+		output = "Buzz"
+	}
 	return
 }
